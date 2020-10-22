67465228f1194eeb670c8938906e7a40c9525c31
laluwathoni <laluwathoni10@gmail.com>
2020-10-22 01:46:57 -0700

    update controller mahasiswa

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -7,6 +7,12 @@ class mahasiswa extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('mahasiswa_index');
+		$this->load->view('mahasiswa/v_index');
+	}
+
+
+	public function create()
+	{
+		$this->load->view('mahasiswa/v_create');
 	}
 }
